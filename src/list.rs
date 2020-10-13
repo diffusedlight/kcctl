25c35cb30260bbaec8cd4ed926d59faeeacf5277
Ben Sayers <bsayers2302@outlook.com>
2020-10-13 20:48:32 +0100

    Added List command functionality

--- src/list.rs
+++ src/list.rs
@@ -1,14 +1,12 @@
-pub mod list {
-  use std::fs;
-  use std::io; 
-  use std::path;
+use std::fs;
+use std::io;
+use std::path;
 
-  pub fn list_configs(config_dir_path: path::PathBuf) -> io::Result<()> {
+pub fn list_configs(config_dir_path: path::PathBuf) -> io::Result<()> {
     for entry in fs::read_dir(config_dir_path)? {
         let dir = entry?;
-        let mut file_name = dir.file_name().into_string().unwrap();
+        let file_name = dir.file_name().into_string().unwrap();
         println!("{}", file_name);
     }
     Ok(())
-  }
 }
