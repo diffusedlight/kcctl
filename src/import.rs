0b7fed9a7af5dbfb8aaecaaaed1e5cfc9838091a
Ben Sayers <bsayers@ansgroup.co.uk>
2020-12-03 13:47:38 +0000

    feat: added import functionality

--- src/import.rs
+++ src/import.rs
@@ -2,9 +2,9 @@ use std::fs;
 use std::path;
 
 pub fn import_config(config_dir_path: path::PathBuf, kubeconfig_path: path::PathBuf, config_name: String) {
-  let name = &config_name.to_lowercase().trim(); 
+  let name = String::from(config_name); 
   let mut config_path = config_dir_path;
-  config_path.push(&name);
+  config_path.push(&name.trim().to_lowercase());
   match fs::copy(kubeconfig_path, config_path) {
     Ok(_t) => println!("Import Success"),
     Err(e) => eprintln!("Import Failed: {}", e)
